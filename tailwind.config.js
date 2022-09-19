cd3a330202bd420435c773ed0f9ce8787dc75b96
Stefan <stefan@hawser.org>
2022-09-19 09:32:57 -0400

    Basic landing page setup
    Basic landing page setup

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      colors: {
+        brightred: 'hsl(12, 88%, 59%)',
+        brightRedLight: 'hsl(12, 18%, 69%',
+        brighRedSuperLight: 'hsl(12, 88%, 95%)',
+        darkBlue: 'hsl(228, 39%, 23%)',
+        darkGreyishBlue: 'hsl(227, 12%, 61%)',
+        veryDarkBlue: 'hsl(233, 12%, 13%)',
+        veryPaleRed: 'hsl(13, 100%, 96%)',
+        veryLightGrey: 'hsl(0, 0%, 98%)',
+      }
+    },
+  },
+  plugins: [],
+}
